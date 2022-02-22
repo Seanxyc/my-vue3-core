af98c114c7c8cfa439b9ddaa356305b71d10ed61
Seanxyc <seanxyc41@gmail.com>
2022-02-22 23:18:06 +0800

    es6

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: seanchen
  * @Date: 2022-02-16 22:50:17
- * @LastEditTime: 2022-02-16 22:53:40
+ * @LastEditTime: 2022-02-21 23:55:34
  * @LastEditors: seanchen
  * @Description:
  */
