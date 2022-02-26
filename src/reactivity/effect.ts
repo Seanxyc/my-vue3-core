93c0cb1ff0346b186e76226e3a6236321c3d6bc8
Seanxyc <seanxyc41@gmail.com>
2022-02-26 18:32:54 +0800

    effect return runner

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,52 +1,53 @@
 /*
  * @Author: seanchen
  * @Date: 2022-02-23 22:35:59
- * @LastEditTime: 2022-02-23 22:35:59
+ * @LastEditTime: 2022-02-26 18:32:07
  * @LastEditors: seanchen
  * @Description:
  */
 
-let activeEffect
+let activeEffect;
 
 class ReactiveEffect {
-  private _fn: any
+  private _fn: any;
 
   constructor(fn) {
-    this._fn = fn
+    this._fn = fn;
   }
 
   run() {
-    activeEffect = this
-    this._fn()
+    activeEffect = this;
+    return this._fn();
   }
 }
 
-const targetMap = new Map()
+const targetMap = new Map();
 export function track(target, key) {
-  let depsMap = targetMap.get(target)
+  let depsMap = targetMap.get(target);
   if (!depsMap) {
-    depsMap = new Map()
-    targetMap.set(target, depsMap)
+    depsMap = new Map();
+    targetMap.set(target, depsMap);
   }
 
-  let dep = depsMap.get(key)
+  let dep = depsMap.get(key);
   if (!dep) {
-    dep = new Set()
-    depsMap.set(key, dep)
+    dep = new Set();
+    depsMap.set(key, dep);
   }
 
-  dep.add(activeEffect)
+  dep.add(activeEffect);
 }
 
 export function trigger(target, key) {
-  const depsMap = targetMap.get(target)
-  const dep = depsMap.get(key)
+  const depsMap = targetMap.get(target);
+  const dep = depsMap.get(key);
   for (const effect of dep) {
-    effect.run()
+    effect.run();
   }
 }
 
 export function effect(fn) {
-  const _effect = new ReactiveEffect(fn)
-  _effect.run()
+  const _effect = new ReactiveEffect(fn);
+  _effect.run();
+  return _effect.run.bind(_effect);
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -1,21 +1,41 @@
-import { reactive } from '../reactive'
-import { effect } from '../effect'
+/*
+ * @Author: seanchen
+ * @Date: 2022-02-26 18:02:28
+ * @LastEditTime: 2022-02-26 18:09:09
+ * @LastEditors: seanchen
+ * @Description:
+ */
+import { reactive } from "../reactive";
+import { effect } from "../effect";
 
-describe('effect', () => {
-  it('happy path', () => {
+describe("effect", () => {
+  it("happy path", () => {
     const user = reactive({
-      age: 10
-    })
+      age: 10,
+    });
 
-    let nextAge
+    let nextAge;
     effect(() => {
-      nextAge = user.age + 1
-    })
+      nextAge = user.age + 1;
+    });
 
-    expect(nextAge).toBe(11)
+    expect(nextAge).toBe(11);
 
     // update
-    user.age++
-    expect(nextAge).toBe(12)
-  })
-})
+    user.age++;
+    expect(nextAge).toBe(12);
+  });
+
+  it("should return runner when call effect", () => {
+    let foo = 10;
+    const runner = effect(() => {
+      foo++;
+      return "foo";
+    });
+
+    expect(foo).toBe(11);
+    const r = runner();
+    expect(foo).toBe(12);
+    expect(r).toBe("foo");
+  });
+});
