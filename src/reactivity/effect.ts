b271c673eff2a2cebde279f8f151527cb1f7d09f
Seanxyc <seanxyc41@gmail.com>
2022-05-22 22:02:14 +0800

    computed

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -10,7 +10,7 @@ import { extend } from "../shared";
 let activeEffect;
 let shouldTrack;
 
-class ReactiveEffect {
+export class ReactiveEffect {
   private _fn: any;
   public scheduler: Function | undefined;
   onStop?: () => void;
