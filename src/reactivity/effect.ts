a06b52d254f33dfb8f1b591ea64769a71c229fba
Seanxyc <seanxyc41@gmail.com>
2022-06-03 23:41:54 +0800

    format file

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -5,121 +5,121 @@
  * @LastEditors: seanchen
  * @Description:
  */
-import { extend } from "../shared";
+import { extend } from '../shared'
 
-let activeEffect;
-let shouldTrack;
+let activeEffect
+let shouldTrack
 
 export class ReactiveEffect {
-  private _fn: any;
-  public scheduler: Function | undefined;
-  onStop?: () => void;
-  deps = [];
-  active = true;
+  private _fn: any
+  public scheduler: Function | undefined
+  onStop?: () => void
+  deps = [        ]
+  active = true
 
   constructor(fn, scheduler?: Function) {
-    this._fn = fn;
-    this.scheduler = scheduler;
+    this._fn = fn
+    this.scheduler = scheduler
   }
 
   run() {
     if (!this.active) {
-      return this._fn();
+      return this._fn()
     }
 
-    shouldTrack = true;
-    activeEffect = this;
-    const result = this._fn();
+    shouldTrack = true
+    activeEffect = this
+    const result = this._fn()
 
     // reset
-    shouldTrack = false;
+    shouldTrack = false
 
-    return result;
+    return result
   }
 
   stop() {
     // 判断是否需要清空deps
     if (this.active) {
-      cleanupEffect(this);
+      cleanupEffect(this)
       if (this.onStop) {
-        this.onStop();
+        this.onStop()
       }
-      this.active = false;
+      this.active = false
     }
   }
 }
 
-let targetMap = new Map();
+let targetMap = new Map()
 export function track(target, key) {
-  if (!isTracking()) return;
+  if (!isTracking()) return
 
   // target -> key -> dep
-  let depsMap = targetMap.get(target);
+  let depsMap = targetMap.get(target)
   if (!depsMap) {
     // 初始化
-    depsMap = new Map();
+    depsMap = new Map()
     // 建立映射关系
-    targetMap.set(target, depsMap);
+    targetMap.set(target, depsMap)
   }
 
-  let dep = depsMap.get(key);
+  let dep = depsMap.get(key)
   if (!dep) {
-    dep = new Set();
-    depsMap.set(key, dep);
+    dep = new Set()
+    depsMap.set(key, dep)
   }
 
-  if (dep.has(activeEffect)) return; // 已经在dep中，不需要重复收集
+  if (dep.has(activeEffect)) return // 已经在dep中，不需要重复收集
 
-  trackEffects(dep);
+  trackEffects(dep)
 }
 
 export function trackEffects(dep) {
-  dep.add(activeEffect);
-  activeEffect.deps.push(dep);
+  dep.add(activeEffect)
+  activeEffect.deps.push(dep)
 }
 
 export function isTracking() {
-  return shouldTrack && activeEffect !== undefined;
+  return shouldTrack && activeEffect !== undefined
 }
 
 export function trigger(target, key) {
-  const depsMap = targetMap.get(target);
-  const dep = depsMap.get(key);
+  const depsMap = targetMap.get(target)
+  const dep = depsMap.get(key)
 
-  triggerEffects(dep);
+  triggerEffects(dep)
 }
 
 export function triggerEffects(dep) {
   for (const effect of dep) {
     if (effect.scheduler) {
-      effect.scheduler();
+      effect.scheduler()
     } else {
-      effect.run();
+      effect.run()
     }
   }
 }
 
 export function stop(runner) {
-  runner.effect.stop();
+  runner.effect.stop()
 }
 
 // 清空deps
 function cleanupEffect(effect) {
   effect.deps.forEach((dep: any) => {
-    dep.delete(effect);
-  });
-  effect.deps.length = 0;
+    dep.delete(effect)
+  })
+  effect.deps.length = 0
 }
 
 export function effect(fn, options: any = {}) {
-  const _effect = new ReactiveEffect(fn, options.scheduler);
-  extend(_effect, options);
+  const _effect = new ReactiveEffect(fn, options.scheduler)
+  extend(_effect, options)
 
-  _effect.run();
+  _effect.run()
 
-  const runner: any = _effect.run.bind(_effect);
-  runner.effect = _effect;
+  const runner: any = _effect.run.bind(_effect)
+  runner.effect = _effect
 
   // fn 返回值
-  return runner;
+  return runner
 }
