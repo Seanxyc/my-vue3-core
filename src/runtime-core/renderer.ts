62e6bf7e8d9c507567d8bbc5112f31855f00d6df
Seanxyc <seanxyc41@gmail.com>
2023-01-15 17:30:30 +0800

    feat: ShapFlag

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,4 +1,5 @@
 import { isObject } from "../shared/index"
+import { ShapeFlags } from "../shared/ShapeFlags"
 import { createComponentInstance, setupComponent } from "./component"
 
 export function render(vnode: any, container: any) {
@@ -7,10 +8,12 @@ export function render(vnode: any, container: any) {
 }
 
 function patch(vnode, container) {
-  if (typeof vnode.type === 'string') {
+  // ShapeFlags   element | stateful component
+  const { shapeFlag } = vnode
+  if (shapeFlag & ShapeFlags.ELEMENT) { // 与运算比较
     // 处理element类型
     processElement(vnode, container)
-  } else if (isObject(vnode.type)) {
+  } else if (shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
     // 处理组件类型
     propcessComponent(vnode, container)
   }
@@ -23,12 +26,12 @@ function processElement(vnode: any, container: any) {
 function mountElement(vnode: any, container: any) {
   // vnode -> element
   const el = (vnode.el = document.createElement(vnode.type)) // string | array
-  const { children, props } = vnode
+  const { children, props, shapeFlag } = vnode
 
-  if (typeof children === "string") {
+  // text children | array children
+  if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
     el.textContent = children
-    // props
-  } else if (Array.isArray(children)) {
+  } else if (shapeFlag * ShapeFlags.ARRAY_CHILDREN) {
     // 每个child调用patch
     mountChildren(children, el)
   }
--- src/runtime-core/vnode.ts
+++ src/runtime-core/vnode.ts
@@ -1,10 +1,25 @@
+import { ShapeFlags } from "../shared/ShapeFlags"
+
 export function createVNode(type, props?, children?) {
   const vnode = {
     type,
     props,
     children,
+    shapeFlag: getShapeFlag(type),
     el: null
   }
 
+  debugger
+
+  if (typeof children === "string") {
+    vnode.shapeFlag |= ShapeFlags.TEXT_CHILDREN // 或运算
+  } else if (Array.isArray(children)) {
+    vnode.shapeFlag |= ShapeFlags.ARRAY_CHILDREN
+  }
+
   return vnode
+}
+
+function getShapeFlag(type) {
+  return typeof type === "string" ? ShapeFlags.ELEMENT : ShapeFlags.STATEFUL_COMPONENT
 }
