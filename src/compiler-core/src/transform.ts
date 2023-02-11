6bc5e84b33955103d8b2da23ac2f5b1aaaa891e1
Seanxyc <seanxyc41@gmail.com>
2023-02-11 21:41:00 +0800

    feat: transform

---
+++ src/compiler-core/src/transform.ts
@@ -0,0 +1,37 @@
+export function transform(root, options) {
+
+  const context = createTransformsContext(root, options)
+  // 深度优先遍历
+  traverseNode(root, context)
+}
+
+function traverseNode(node: any, context: any) {
+  const { nodeTransforms } = context
+  for (let i = 0; i < nodeTransforms.length; i++) {
+    const transform = nodeTransforms[i]
+    transform(node)
+  }
+
+  traverseChildren(node, context)
+}
+
+function traverseChildren(node: any, context: any) {
+  const children = node.children
+
+  if (children) {
+    for (let i = 0; i < children.length; i++) {
+      const node = children[i]
+      traverseNode(node, context)
+    }
+  }
+}
+
+function createTransformsContext(root: any, options: any) {
+  const context = {
+    root,
+    nodeTransforms: options.nodeTransforms || []
+  }
+
+  return context
+}
+
